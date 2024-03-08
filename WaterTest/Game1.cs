7c7fdcd0aa241215e38e678e972a1695613c6238
Black Chowder <gman.dude132@gmail.com>
2024-03-07 23:45:47 -0500

    Time manipulation with mouse control

--- WaterTest/Game1.cs
+++ WaterTest/Game1.cs
@@ -15,24 +15,26 @@ namespace WaterTest
         //Misc Water Parameters/Variables
         private const float Damping = 0.99f;
         private const int BrushSize = 1;
-        private const float TimerStep = 0.01f;
 
         //CPU Implementation variables
         private float[,] prevData;
         private float[,] curData;
+        private float cpuTimeAccumulator = 0f;
         private RenderTarget2D cpuRt;
 
         //Shader implementation variables
         private Effect WaterEffect;
         private RenderTarget2D prevRt;
         private RenderTarget2D curRt;
+        private RenderTarget2D newFrame;
+        private float shaderTimeAccumulator = 0f;
 
         //Define screen size
         private Point iResolution = new Point(100, 100); //This is the size the effects will be rendered at
         private const int ResolutionScaler = 8; //This is the scale at which the effects are being blown up to fill the screen
 
+        //Time manipulation variables
         private Point prevMousePos;
-        private float timeAccumulator = 0f;
         private float timeMod = 0f;
         private const float timeStep = 16f;
 
@@ -58,6 +60,7 @@ namespace WaterTest
             //Initialize render targets for shader implementation
             curRt = new RenderTarget2D(GraphicsDevice, iResolution.X, iResolution.Y);
             prevRt = new RenderTarget2D(GraphicsDevice, iResolution.X, iResolution.Y);
+            newFrame = curRt;
 
             prevMousePos = Mouse.GetState().Position;
 
@@ -82,6 +85,7 @@ namespace WaterTest
             Point mousePos = mouseState.Position;
             bool isMousePressed = mouseState.LeftButton == ButtonState.Pressed;
 
+            //Handle time manipulation
             timeMod = (prevMousePos - mousePos).ToVector2().Length();
             prevMousePos = mousePos;
 
@@ -116,15 +120,8 @@ namespace WaterTest
 
         protected override void Draw(GameTime gameTime)
         {
-            timeAccumulator += timeMod;
-            
-            while (timeAccumulator > timeStep)
-            {
-                timeAccumulator -= timeStep;
-                UpdateCPU();
-            }
-
-            RenderTarget2D newFrame = UpdateShader();
+            UpdateCPU();
+            UpdateShader();
 
             //Output Results To Screen
             GraphicsDevice.SetRenderTarget(null);
@@ -148,52 +145,77 @@ namespace WaterTest
 
         private void UpdateCPU()
         {
-            //Update curData and prevData and set colorData accordingly
-            Color[] colorData = new Color[iResolution.X * iResolution.Y];
-            for (int i = 1; i < iResolution.X - 1; i++)
-            {
-                for (int j = 1; j < iResolution.Y - 1; j++)
+            for (cpuTimeAccumulator += timeMod; cpuTimeAccumulator > timeStep; cpuTimeAccumulator -= timeStep)
+            {                
+                //Update curData and prevData and set colorData accordingly
+                Color[] colorData = new Color[iResolution.X * iResolution.Y];
+                for (int i = 1; i < iResolution.X - 1; i++)
                 {
-                    curData[i, j] = (
-                        prevData[i - 1, j] + prevData[i + 1, j] + prevData[i, j - 1] + prevData[i, j + 1]) / 2 - curData[i, j];
+                    for (int j = 1; j < iResolution.Y - 1; j++)
+                    {
+                        curData[i, j] = (
+                            prevData[i - 1, j] + prevData[i + 1, j] + prevData[i, j - 1] + prevData[i, j + 1]) / 2 - curData[i, j];
 
-                    curData[i, j] *= Damping;
+                        curData[i, j] *= Damping;
 
-                    int index = (i + j * iResolution.X);
-                    colorData[index] = new Color(curData[i, j], curData[i, j], curData[i, j], 1.0f);
+                        int index = (i + j * iResolution.X);
+                        colorData[index] = new Color(curData[i, j], curData[i, j], curData[i, j], 1.0f);
+                    }
                 }
+
+                //Swap
+                (curData, prevData) = (prevData, curData);
+
+                //Assign colorData to output render target
+                cpuRt.SetData(colorData);
             }
 
-            //Swap
-            (curData, prevData) = (prevData, curData);
-
-            //Assign colorData to output render target
-            cpuRt.SetData(colorData);
         }
 
-        private RenderTarget2D UpdateShader()
+        private void UpdateShader()
         {
-            //Set effect parameters
-            WaterEffect.Parameters["iResolution"].SetValue(iResolution.ToVector2());
-            WaterEffect.Parameters["damping"].SetValue(Damping);
-            WaterEffect.Parameters["Previous"].SetValue(prevRt);
+            shaderTimeAccumulator += timeMod;
 
-            //Calculate new frame
-            RenderTarget2D newFrame = new RenderTarget2D(GraphicsDevice, iResolution.X, iResolution.Y);
-            GraphicsDevice.SetRenderTarget(newFrame);
-            GraphicsDevice.Clear(Color.Black);
-            spriteBatch.Begin(effect: WaterEffect);
-            spriteBatch.Draw(curRt, Vector2.Zero, Color.White);
-            spriteBatch.End();
+            //When not time to itterate effect, render new frame without swapping or modifying previous or current render targets
+            if (shaderTimeAccumulator <= timeStep)
+            {
+                //Set effect parameters
+                WaterEffect.Parameters["iResolution"].SetValue(iResolution.ToVector2());
+                WaterEffect.Parameters["damping"].SetValue(Damping);
+                WaterEffect.Parameters["Previous"].SetValue(prevRt);
 
-            //Set current render target to new frame
-            curRt.Dispose();
-            curRt = newFrame;
+                //Calculate new frame
+                newFrame = new RenderTarget2D(GraphicsDevice, iResolution.X, iResolution.Y);
+                GraphicsDevice.SetRenderTarget(newFrame);
+                GraphicsDevice.Clear(Color.Black);
+                spriteBatch.Begin(effect: WaterEffect);
+                spriteBatch.Draw(curRt, Vector2.Zero, Color.White);
+                spriteBatch.End();
+            }
 
-            //Swap
-            (curRt, prevRt) = (prevRt, curRt);
+            //Normal itteration effect with current render target assignment and render target swapping6
+            for (; shaderTimeAccumulator > timeStep; shaderTimeAccumulator -= timeStep)
+            {
+                //Set effect parameters
+                WaterEffect.Parameters["iResolution"].SetValue(iResolution.ToVector2());
+                WaterEffect.Parameters["damping"].SetValue(Damping);
+                WaterEffect.Parameters["Previous"].SetValue(prevRt);
 
-            return newFrame;
+                //Calculate new frame
+                newFrame = new RenderTarget2D(GraphicsDevice, iResolution.X, iResolution.Y);
+                GraphicsDevice.SetRenderTarget(newFrame);
+                GraphicsDevice.Clear(Color.Black);
+                spriteBatch.Begin(effect: WaterEffect);
+                spriteBatch.Draw(curRt, Vector2.Zero, Color.White);
+                spriteBatch.End();
+
+                //Set current render target to new frame
+                curRt.Dispose();
+                curRt = newFrame;
+
+                //Swap
+                (curRt, prevRt) = (prevRt, curRt);
+            }
         }
 
         //Helper function to create quick texture
